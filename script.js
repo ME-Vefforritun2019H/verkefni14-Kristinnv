2480a03ea041f0e7c80b00594e1fecb21d35de7f
kristinn <kristinnv812@gmail.com>
2019-11-10 13:11:18 +0000

    Búinn

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
+let greeting = "Howdy ";
+let name = "Molly";
+let message = ", please check your order:";
+let welcome = greeting + name + welcome;
 
+let sign = "Montague House"; // => 14 á lengdina
+let tiles = sign.length; // lengdin á sign
+subTotal = tiles * 5;
+let shipping = 7;
+let grandTotal = subTotal + shipping;
+
+let el =  document.getElementById("greeting");
+el.textContent = Welcome;
+
+let elSign = document.getElementById("userSign");
+elSign.textContent = tiles
+
+let elTiles = document.getElementById(tiles);
+elTiles.textContent = tiles;
+
+let elSubTotal = document.getElementById("subTotal");
+elSubTotal.textContent = "$" + suTotal;
+
+let elShipping = document.getElementById("shipping");
+elShipping.textContent = "$" + shipping;
+
+let elGrandTotal = document.getElementById("grandTotal");
+elGrandTotal.textContent = "$" + grandTotal
